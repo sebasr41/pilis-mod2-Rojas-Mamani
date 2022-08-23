df72c540b59f3a7acbfc060a1615620f22487bf5
Gabriel <gamcode98@gmail.com>
2022-08-23 09:17:11 -0300

    add middle and large view, resize and scroll events

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
 const d = document;
+const w = window;
 const $btn = d.getElementById("menu");
 const $menu = d.querySelector(".navbar-menu");
 
 $btn.addEventListener("click", () => {
-  $btn.classList.toggle("fa-xmark");
+  $btn.classList.contains("fa-bars")
+    ? $btn.classList.replace("fa-bars", "fa-xmark")
+    : $btn.classList.replace("fa-xmark", "fa-bars");
+
   $menu.classList.toggle("isActive");
 });
+
+const hideMenu = (eventToListen) => {
+  w.addEventListener(eventToListen, () => {
+    $btn.classList.replace("fa-xmark", "fa-bars");
+    $menu.classList.remove("isActive");
+  });
+};
+
+hideMenu("scroll");
+hideMenu("resize");
