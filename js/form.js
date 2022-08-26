78315299f6298891012cc8870b6c2d1854d830de
sebasr41 <sebasr432@gmail.com>
2022-08-26 17:04:21 -0300

    agregando validaciones al formulario

--- js/form.js
+++ js/form.js
@@ -1,38 +1,57 @@
-function onClick (event) {
-    event.preventDefault();
+/* form post y validation */
+function onClick(event) {
+  event.preventDefault();
 
-    const mensaje = {
-      comercio: document.getElementById('comercio').value,
-      email: document.getElementById('email').value,
-    }
- 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify(mensaje),
-      headers: { "Content-type": "application/json; charset=UTF-8" },
+  const mensaje = {
+    comercio: document.getElementById("comercio").value,
+    email: document.getElementById("email").value,
+    telefono: document.getElementById("telefono").value,
+  };
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(mensaje),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      Swal.fire("Enviado", "Gracias por registrarse", "success");
+      cleanForm();
+      document.getElementById("enviar").disabled=true;
     })
-      .then((response) => response.json())
-      .then((json) => { 
-          console.log(json);
-          Swal.fire(
-              'Enviado',
-              'Gracias por registrarse',
-              'success'
-          );
-          cleanForm();
-      })
-      .catch((err) => console.log(err));
-  
-  }
-  function cleanForm() {
-    let formulario = document.getElementById('formulario');    
-    formulario.reset();    
+    .catch((err) => {
+      Swal.fire("Enviado", "ocurrio un error inesperado", "error");
+    });
 }
-function redirectUrl(){
-    window.location.href = "https://google.com";    
+function cleanForm() {
+  let formulario = document.getElementById("formulario");
+  formulario.reset();
+}
+function redirectUrl() {
+  window.location.href = "https://google.com";
 }
 
 let boton = document.getElementById("enviar");
 boton.addEventListener("click", onClick);
 
-  
+const inputs = document.querySelectorAll(".form [required]");
+inputs.forEach((input) => {
+  const span = document.createElement("span");
+  span.id = input.name;
+  span.textContent = input.title;
+  span.classList.add("error-form", "isHide");
+  input.insertAdjacentElement("afterend", span);
+});
+document.addEventListener("keyup", (event) => {
+  if (event.target.matches(".form [required]")) {
+    let input = event.target;
+    let pattern = input.pattern;
+    if (pattern && input.value !== "") {
+      let regex = new RegExp(pattern);
+      return !regex.exec(input.value)
+        ? (document.getElementById(input.name).nextElementSibling.classList.add("isActive"), document.getElementById("enviar").disabled=true)
+        : (document.getElementById(input.name).nextElementSibling.classList.remove("isActive"), document.getElementById("enviar").disabled=false)
+    }
+  }
+});
